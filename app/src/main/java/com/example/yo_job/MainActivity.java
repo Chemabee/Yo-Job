6ed99e76487b8aa08a802b52c4e5e9b49e3c142c
Chemabee <32854067+Chemabee@users.noreply.github.com>
2019-02-10 19:19:43 +0000

    Con exclamacion

--- app/src/main/java/com/example/yo_job/MainActivity.java
+++ app/src/main/java/com/example/yo_job/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent login = new Intent(MainActivity.java, SignupActivity.java);
+        startActivity(login);
     }
 }
