6b547b4f2153b3eaa8b6cb333689257c39f85de5
Chemabee <32854067+Chemabee@users.noreply.github.com>
2019-02-19 16:04:09 +0000

    meh

--- app/src/main/java/com/example/yo_job/JobsActivity.java
+++ app/src/main/java/com/example/yo_job/JobsActivity.java
@@ -12,6 +12,7 @@ public class JobsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jobs);
     }
+    /*
     //Search a Job button
     public void search (View v){
         Intent searchI = new Intent(this, SearchActivity.class);
@@ -22,30 +23,5 @@ public class JobsActivity extends AppCompatActivity {
     public void post (View v){
         Intent postI = new Intent(this, PostAJobActivity.class);
         startActivity(postI);
-    }
-
-    public void jobsNavBar (View v) {
-        Intent jobs = new Intent(this, JobsActivity.class);
-        startActivity(jobs);
-    }
-
-    public void searchNavBar (View v) {
-        Intent search = new Intent(this, SearchActivity.class);
-        startActivity(search);
-    }
-
-    public void postNavBar (View v) {
-        Intent post = new Intent(this, PostAJobActivity.class);
-        startActivity(post);
-    }
-
-    public void messagesNavBar (View v) {
-        Intent messages = new Intent(this, MessagesActivity.class);
-        startActivity(messages);
-    }
-
-    public void profileNavBar (View v) {
-        Intent profile = new Intent(this, ProfileActivity.class);
-        startActivity(profile);
-    }
+    }*/
 }
--- app/src/main/java/com/example/yo_job/SignupActivity.java
+++ app/src/main/java/com/example/yo_job/SignupActivity.java
@@ -1,7 +1,9 @@
 package com.example.yo_job;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+
+    public void register (View view){
+        Intent i = new Intent (this, JobsActivity.class);
+        startActivity(i);
     }
 }
