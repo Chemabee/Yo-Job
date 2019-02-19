69c98a987c3563e9bf46158893af26dceedb17a5
gojimenez <33057398+gojimenez@users.noreply.github.com>
2019-02-19 19:09:22 +0000

    bla bla bla

--- app/src/main/java/com/example/yo_job/ListOfJobsActivity.java
+++ app/src/main/java/com/example/yo_job/ListOfJobsActivity.java
@@ -1,20 +1,13 @@
 package com.example.yo_job;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class SearchActivity extends AppCompatActivity {
-
+public class ListOfJobsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
+        setContentView(R.layout.activity_list_of_jobs);
     }
-
-    
-
-
-
 }
