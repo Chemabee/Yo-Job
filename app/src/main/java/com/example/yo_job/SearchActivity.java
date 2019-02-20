9e4fd74cce3a2ee1f5153fce5368f7647d00d61d
gojimenez <33057398+gojimenez@users.noreply.github.com>
2019-02-20 16:49:36 +0000

    search relacionado con lista de trabajos

--- app/src/main/java/com/example/yo_job/SearchActivity.java
+++ app/src/main/java/com/example/yo_job/SearchActivity.java
@@ -2,6 +2,7 @@ package com.example.yo_job;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.support.v7.app.AppCompatActivity;
 
 public class SearchActivity extends AppCompatActivity {
@@ -13,7 +14,10 @@ public class SearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search);
     }
 
-    
+    public void search (View v) {
+        Intent search = new Intent(this, ListOfJobsActivity.class);
+        startActivity(search);
+    }
 
 
 
