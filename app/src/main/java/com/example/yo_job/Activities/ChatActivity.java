2fcd5013dc78c4a5952947eae415d59de2284163
gojimenez <33057398+gojimenez@users.noreply.github.com>
2019-03-19 17:44:07 +0000

    reorganizado y bien mostrado nombre en los mensajes

--- app/src/main/java/com/example/yo_job/Activities/ChatActivity.java
+++ app/src/main/java/com/example/yo_job/Activities/ChatActivity.java
@@ -1,5 +1,6 @@
 package com.example.yo_job.Activities;
 
+import android.graphics.Typeface;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -10,19 +11,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.yo_job.SimpleClasses.Message;
 import com.example.yo_job.Messages.MessageReceive;
 import com.example.yo_job.Messages.MessageSend;
 import com.example.yo_job.Messages.MessagesAdapter;
 import com.example.yo_job.R;
+import com.example.yo_job.SimpleClasses.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ServerValue;
+import com.google.firebase.database.ValueEventListener;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -33,11 +38,13 @@ public class ChatActivity extends AppCompatActivity {
     private RecyclerView rvMessages;
     private EditText txtMessages;
     private Button btnSend;
+    private String senderName;
 
     private MessagesAdapter adapter;
 
     private FirebaseDatabase database;
     private DatabaseReference databaseReference;
+    private DatabaseReference userDatabaseReference;
     private FirebaseAuth auth;
     private String receiverID;
     private String receiverName;
@@ -65,16 +72,35 @@ public class ChatActivity extends AppCompatActivity {
 
         database = FirebaseDatabase.getInstance();
         databaseReference = database.getReference("Chat");
+        userDatabaseReference = database.getReference("Users");
 
         adapter = new MessagesAdapter(this);
         LinearLayoutManager l = new LinearLayoutManager(this);
         rvMessages.setLayoutManager(l);
         rvMessages.setAdapter(adapter);
 
+        Query getSenderName = userDatabaseReference.orderByKey().equalTo(auth.getCurrentUser().getUid());
+        getSenderName.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                        User u = ds.getValue(User.class);
+                        senderName = u.getName();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(ChatActivity.this,"ERROR in Query",Toast.LENGTH_LONG).show();
+            }
+        });
+
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                newMessage(auth.getCurrentUser().getUid(), receiverID, new MessageSend(txtMessages.getText().toString(), auth.getCurrentUser().getDisplayName(), ServerValue.TIMESTAMP));
+                newMessage(auth.getCurrentUser().getUid(), receiverID, new MessageSend(txtMessages.getText().toString(), senderName, ServerValue.TIMESTAMP));
                 txtMessages.setText("");
             }
         });
