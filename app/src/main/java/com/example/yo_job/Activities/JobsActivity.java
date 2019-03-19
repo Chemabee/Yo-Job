0d9d497d7f1d44b66e1f3751640f855292d398b6
gojimenez <33057398+gojimenez@users.noreply.github.com>
2019-03-19 16:57:41 +0000

    hecho menu, a reorganizar los layouts

--- app/src/main/java/com/example/yo_job/Activities/JobsActivity.java
+++ app/src/main/java/com/example/yo_job/Activities/JobsActivity.java
@@ -3,6 +3,8 @@ package com.example.yo_job.Activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.example.yo_job.R;
@@ -23,8 +25,38 @@ public class JobsActivity extends AppCompatActivity {
 
     //Post a job button
     public void post (View v){
-        Intent postI = new Intent(this, MessagesActivity.class);
+        Intent postI = new Intent(this, PostAJobActivity.class);
         startActivity(postI);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.messages) {
+            Intent messages = new Intent(this, MessagesActivity.class);
+            startActivity(messages);
+            return true;
+        }
+
+        if (id == R.id.profile) {
+            Intent profile = new Intent(this, ProfileActivity.class);
+            startActivity(profile);
+            return true;
+        }
+
+        if (id == R.id.exit) {
+            finish();
+            moveTaskToBack(true);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
